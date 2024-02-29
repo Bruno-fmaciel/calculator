d37b29a2a7acdb20ef243f5e83bdb5b364496a87
Bruno-fmaciel <bfma@cesar.school>
2024-02-29 07:17:19 -0300

    Add operate function

--- script.js
+++ script.js
@@ -11,5 +11,28 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b === 0) {
+        return "Can't divide a number by zero!";
+    } else {
+        return a / b;
+    }
 }
+
+let variable1;
+let variable2;
+let operator;
+
+function operate(variable1, variable2, operator) {
+    if (operator == "+") {
+        return add(variable1, variable2);
+    } else if (operator == "-") {
+        return subtract(variable1, variable2);
+    } else if (operator == "*") {
+        return multiply(variable1, variable2)
+    } else if (operator == "/") {
+        return divide(variable1, variable2)
+    } else {
+        return "Invalid operator!"
+    }
+}
+
